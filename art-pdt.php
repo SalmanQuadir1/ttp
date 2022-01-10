72fb7f01bdb152037f828ce410285bc9c0e38d35
shah-Shahid <shahid.sheikhpora@gmail.com>
2022-01-10 15:13:47 +0530

    new page art-pdt added

---
+++ art-pdt.php
@@ -0,0 +1,163 @@
+<?php
+  require_once('header.php');
+  $activePage = "Services";
+?>
+
+<section id="main-section" class="clearfix">
+	<div class="container pt-5">
+<!-- Body Section -->
+
+     
+<section id="about" class="about privacy-policy" >
+
+    <div class="container">
+        <div class="row" data-aos="fade-up">          
+
+            <div class="col-lg-12 col-md-12" >
+                <div class="about-content" data-aos="fade-left" >
+                    <h2>Administered Antigen Rapid Test (ART)</h2>
+                    <p>
+                        
+                        <div class="table-responsive">
+                            <table class="table table-borderless">
+                                <div class="tbody">
+                                    <tr>
+                                        <th>What</th>
+                                        <td>Professionally Administered Antigen Rapid Test (ART)</td>
+                                        <td rowspan="6">
+                                            <img clas="img-fluid " src="assets/img/art-pdt3.png" alt="" data-aos="slide-left" data-aos-delay="100">
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th>For</th>
+                                        <td>Pre-Departure Test / Pre-Event Test</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Where</th>
+                                        <td>Only at Selected Test Centres (see below)</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Price</th>
+                                        <td>S$25.00 per test</td>
+                                    </tr>
+                                    <tr>
+                                        <th>How</th>
+                                        <td>Book Appointment Here -> <a href="book-artpdt.php"> www.thetestingpro.com/book-artpdt </a></td>
+                                    </tr>
+                                    <tr>
+                                        <th>Includes</th>
+                                        <td>Digital Notarized Health Certificate*</td>                                        
+                                    </tr>
+                                    <tr>
+                                        <th>Indemnity</th>
+                                        <td colspan="2">TheTestingPro is only responsible for administering the ART and providing 
+                                            the notarized digital health certificate of test results. It is the client's 
+                                            responsibility to ensure the testing window requirements of the specific 
+                                            travel/event and that the appointment can meet the required timeline, 
+                                            as well as the validity of Administered ART, before taking any such tests 
+                                            at our test centres.</td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+                    </p>
+                </div>
+            </div>
+        
+        </div>
+        <div class="row" >
+            <div class="col-md-12" data-aos="fade-right">
+                <p>
+                    <h4>*About Digital Notarized Health Certificate</h4>
+                        <ul>
+                            <li>The Digital Notarized HealthCerts is issued via email only</li>
+                            <li>You will receive the HealthCert within the day of the completion of the test</li>
+                            <li>All Digital Notarized HealthCerts include the EU-DCC compatible QR code</li>
+                        </ul> 
+                </p>
+            </div>
+            <div class="col-sm-12" data-aos="fade-left">
+                <h4 class="text-center mt-3">Test Centres with Administered ART Service</h4>
+                <div class="table-responsive">
+                    <table class="table table-success table-hover">
+                        <thead>
+                            <tr>
+                                <th>Test Center</th>
+                                <th>Address</th>
+                                <th>Operating Hours</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Pasir Panjang Wholesale Centre <br> (Auction Hall) <br> <span class="text-danger"><i>Administered ART available</i></span></td>
+                                <td>1 Wholesale Centre <br>Singapore 110001</td>
+                                <td><b>Monday to Wednesday only </b><br><tt>8am to 12pm</tt></td>
+                            </tr>
+                            <tr>
+                                <td>Canopy @ J-Link <br> (Jurong East Central) <br> <span class="text-danger"><i>Administered ART available</i></span></td>
+                                <td>134 Jurong Gate Way Road<br>Singapore 600134 <br> (Behind JCube)</td>
+                                <td><b>Monday to Saturday </b><br><tt>9am to 1pm <br> 2pm to 5pm</tt></td>
+                            </tr>
+                            <tr>
+                                <td>Blk 60A Circuit Road   <br>(Event Hall)<br> <span class="text-danger"><i>Administered ART available</i></span></td>
+                                <td>60A Circuit Road<br>Singapore 370060</td>
+                                <td><b>Monday to Friday </b><br><tt>9am to 1pm <br> 2pm to 5pm <br> <b>Saturday</b> <br> 9am to 1pm </tt></td>
+                            </tr>
+                            <tr>
+                                <td>Blk 17 Marsiling Lane   <br>(Marsiling Market) <br> <span class="text-danger"><i>Administered ART available</i></span></td>
+                                <td>17 Marsiling Lane<br>Singapore 730017</td>
+                                <td><b>Monday to Friday </b><br><tt>9am to 1pm <br> 2pm to 5pm <br> <b>Saturday</b> <br> 9am to 1pm </tt></td>
+                            </tr>
+                            <tr>
+                                <td>Blk 234A Bt Panjang Ring Rd    <br>(Event Hall) <br> <span class="text-danger"><i>Administered ART available</i></span></td>
+                                <td>234A Bukit Panjang Ring Road<br>Singapore 671234</td>
+                                <td><b>Monday to Friday </b><br><tt>9am to 1pm <br> 2pm to 5pm <br> <b>Saturday</b> <br> 9am to 1pm </tt></td>
+                            </tr>
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            <div class="col-sm-12" data-aos="slide-up">
+                <p class="text-center font-weight-bold">
+                    All our test centres are licensed by the Ministry of Health (MOH) Singapore to conduct both administered 
+                    and supervised Antigen Rapid Test (ART).<br>
+                    <a href="https://www.moh.gov.sg/covid-19/quick-test-centres-(qtcs)">https://www.moh.gov.sg/covid-19/quick-test-centres-(qtcs)</a> 
+                </p>
+
+                <div class="row mt-4 box form-group">
+                    <div class="col-sm-12">
+                        <h4 class="text-center text-danger"><b>Only S$25.00 per test</b></h4> <br>                           
+                        <p class="font-weight-bold text-center" data-aos="slide-right">
+                        For any enquiries, please email to <a href="mailto:pqtc@thetestingpro.com">pqtc@thetestingpro.com</a>
+                        </p>
+                        <p class="text-center" data-aos="slide-left">
+                            To book an appointment for Administered ART, please <a href="book-artpdt.php" >CLICK HERE</a>
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+</section>
+<!-- End Body Section -->
+</div>
+</section> 
+
+
+</main><!-- End #main -->
+<!-- <script>
+    $(document).ready(function() {
+        
+        
+    });
+</script> -->
+
+<?php
+require_once('footer.php');
+?>
+
