7298de6f9c93ef7cf03fe2f10779af8dbc73b349
PeerFaizanAziz <67686986+PeerFaizanAziz@users.noreply.github.com>
2022-02-13 14:23:20 +0530

    mts

---
+++ mts.php
@@ -0,0 +1,309 @@
+<?php
+  require_once('header.php');
+  $activePage = "Services";
+?>
+
+<section id="main-section" class="clearfix">
+	<div class="container pt-5">
+<!-- Body Section -->
+
+     
+<section id="about" class="about privacy-policy" >
+
+    <div class="container">
+        <div class="row" data-aos="fade-up">          
+
+            <div class="col-lg-12 col-md-12" >
+                <div class="text-center" data-aos="fade-left" >
+                    <h2>Mobile Testing Services (MTS)</h2>
+                </div>
+            </div>
+        
+        </div>
+        <div class="row" >
+            <div class="col-md-12" data-aos="fade-right">
+            <p>
+                    Can’t find time to visit a clinic/hospital or test centre for your Covid-19 tests? Fret<br>
+                    not, <strong>WE SIMPLIFY TESTING</strong> for you.
+                    </p>
+                    <p>
+                    With TheTestingPro<strong> Mobile Testing Services (MTS),</strong> you are just <strong>Three (3) Easy Steps</strong><br>
+                     away from getting the required Covid-19 Test done at the comfort of a location <br>
+                     convenient for you. 
+                    </p>
+            </div>
+
+            //imageeeee<br> <br> <br>
+
+            <div class="col-lg-12 col-md-12" >
+                <div class="text-center" data-aos="fade-left" >
+                    <h2>Mobile Testing Services and Fees</h2>
+                </div>
+            </div>
+
+
+            <div class="col-sm-12" data-aos="fade-left">
+                <h4 class="text-center mt-3">Test Centres with PCR Test Service</h4>
+                <div class="table-responsive">
+                    <table class="table table-success table-hover">
+                        <thead>
+                            <tr>
+                                <th>SN</th>
+                                <th>Test Type</th>
+                                <th>Category</th>
+                                <th>Fees</th>
+                                <th>Remarks</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <td colspan="2">M1</td>
+                            <td colspan="2"></td>
+                            Polymerase Chain Reaction<br>
+                            (PCR) Nasal Swab</td>
+                           
+                            </tr>
+
+                            <tr>
+                            
+                            </tr>
+
+                            <tr>
+                               
+                            </tr>                           
+                            <tr>
+                                
+                            </tr>
+                            <tr>
+
+                            </tr>
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            <div class="col-sm-12" data-aos="slide-up">
+                <p class="text-center">
+                ** Only applicable for additional tests under the same booking on the same day and location<br>
+                    </p>
+
+                <div class="row mt-4 box form-group">
+                    <div class="col-sm-12">
+                                                
+                        <p class="font-weight-bold text-center" data-aos="slide-left">
+    
+                        <h4 class="text-center">
+                            <strong>
+                            All Pre-Departure Tests comes with a Notarised Digital Health Certificate
+                            </strong>
+                        </h4>
+                        </p><br>
+                        <h4 class="text-center text-danger">
+                            <strong>
+                            <a href="www.thetestingpro.com/book-mts" >Click Here To Book For Your Mobile Test</a>
+                            </strong>
+                        </h4> <br>  
+                        
+                    </div>
+                </div>
+
+                <hr>
+                <div class="row mt-4 box form-group">
+                    <div class="col-sm-12">
+                        <h2 class=" text-center">
+                        
+                            <u>IMPORTANT NOTES, TERMS & CONDITIONS</u>
+                            
+                            <br> 
+                        </h2> <br>                           
+                        <p class="font-weight-bold" data-aos="slide-left">
+                            <h4>
+                            Operating Hours
+                            </h4>
+                            <p>
+                            Monday to Sunday:<strong> 9:30 AM – 6:30 PM</strong>
+                            </p> <br> 
+                            
+                            <p>
+                            If you require off-hours test services, please contact our MTS team via email:<br>
+                              <a href="mts@thetestingpro.com ">mts@thetestingpro.com </a>
+                            </p> <br>
+                            <h4>
+                            Appointment Bookings
+                            </h4>
+                            <p>
+                            Bookings for all mobile testing services must be made at least 24 hours in <br>
+                            advance. <br>
+
+                            Bookings that are made <u>less than 24 hours</u> in advance are considered last minute<br> bookings and are subject to a surcharge of $38.00 (per test). To make such last-<br>
+                            minute bookings, please contact our MTS team via email: <a href="mts@thetestingpro.com ">mts@thetestingpro.com </a> 
+
+                            <h4>
+                            Payment
+                            </h4>
+                            <p>
+                            Pre-Payment is <u>required</u> for all mobile test services. We only accept online credit<br>
+                            card payment (Visa, Master, AMEX, Union Pay). Please note that booking will only<br>
+                             be confirmed upon receipt of payment.<br>
+
+                            <i>Note that our testing team <u>will not</u> collect payment onsite for any mobile testing <br>
+                            services rendered.</i>
+
+                            <h4>
+                            Additional Surcharges
+                            </h4>
+                            <p>
+                            <div class="col-sm-12" data-aos="fade-left">
+                <h4 class="text-center mt-3"></h4>
+                <div class="table-responsive">
+                    <table class="table table-success table-hover">
+                        <thead>
+                            <tr>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                        <td>Sundays / Public Holidays</td>
+                        <td> $38.00 per booking</td>
+                            </tr>
+                        <td>Long Haul Locations<br> 
+                       <small><i>(Sentosa, Jurong Island, Changi Airport, Changi Village, Tuas Area)</i></small></td>
+                        <td> $38.00 per booking</td>
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+                            <h4>
+                            Rescheduling
+                            </h4>
+                            <p>
+                            Rescheduling is allowed <u>ONLY</u> if requested more than 24 hours before scheduled<br>
+                            booking time and is subject to an admin fee of<strong> $38.00 </strong>(per test). To reschedule<br>
+                            your appointment, please contact our MTS team via email: <br>
+                              <a href="mts@thetestingpro.com ">mts@thetestingpro.com </a> 
+                            </p> <br>      
+                        </p>
+                        <h4>
+                        Cancellation Policy
+                            </h4>
+                            <p>
+                            We strongly discourage cancellation. A cancellation fee (per test) will be charged<br>
+                            (on the fees pre-paid) as follows: 
+                            </p>       
+                        </p>
+                        <div class="table-responsive">
+                    <table class="table table-success table-hover">
+                        <thead>
+                            <tr>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                        <td>More than 24 hours before scheduled booking</td>
+                        <td>20%</td>
+                            </tr>
+                            <tr>
+                        <td>Between 24 – 12 hours before scheduled booking</td>
+                        <td>50%</td>
+                            </tr>
+                            <tr>
+                        <td>Less than 12 hours before scheduled booking</td>
+                        <td>100%</td>
+                            </tr>
+                            <tr>
+                        <td>No Show / Cancellation on site</td>
+                        <td>100%</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                        
+                    </div>
+                </div>
+                <p>
+                For cancellation and refund matters, please contact our MTS team via email:<br>
+                <a href="mts@thetestingpro.com ">mts@thetestingpro.com </a> 
+                </p>   <br>
+
+                <div class="container">
+        <div class="row" data-aos="fade-up">          
+
+            <div class="col-lg-12 col-md-12" >
+                <div class="" data-aos="fade-left" >
+                    <h3>Testing Area/Zone & Requirements</h3>
+                </div>
+                <p>
+                The area where the test is to be conducted (Testing Zone) should be well-<br>
+                ventilated (as much as possible) and does not obstruct common walkways or<br>
+                 corridors.
+               </p>
+               <p>
+               All personnel in the household must be wearing at least surgical masks when our,<br>
+               testing team enters the premises and when the test procedure is underway. 
+              </p> 
+              <p>
+              All other personnel (except for the one to be tested) should not be in or within<br>
+               proximity of the Testing Zone. 
+              </p>
+              <p>
+              If swab testing is to be done within the premises of a hotel, the test should be<br>
+               conducted discreetly at the doorway of the hotel room.
+              </p>
+              <p>
+              Our testing team reserve the full rights to refuse carrying out the test procedure(s)<br>
+               if the premise or the Testing Zone is deemed to be unsuitable and does not<br>
+               adhere to the Infection Protection Control (IPC) protocols of mobile testing as set<br>
+               out by the Ministry of Health Singapore. 
+              </p> 
+              <p>
+              Should you have any queries about our Mobile Testing Services, please email us:<br>
+              <a href="mts@thetestingpro.com ">mts@thetestingpro.com </a> 
+              </p>
+            </div>
+        
+        </div>
+
+
+                
+                <div class="row mt-4 box form-group">
+                    <div class="col-sm-12">
+                        <h5 class="text-center">
+                            <p>
+                            <strong>
+                            <p class="text-center text-danger" data-aos="slide-right">
+                            <a href="www.thetestingpro.com/book-mts">Click Here To Book For Your Mobile Test</a>
+                        </p>
+
+                            </strong>
+                            </p>
+                        </p>
+                    </div>
+                </div>
+
+               
+            </div>
+        </div>
+
+    </div>
+    
+
+</section>
+<!-- End Body Section -->
+</div>
+</section> 
+
+
+</main><!-- End #main -->
+<!-- <script>
+    $(document).ready(function() {
+        
+        
+    });
+</script> -->
+
+<?php
+require_once('footer.php');
+?>
+
